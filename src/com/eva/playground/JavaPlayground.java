095c6787ac9257f0f2a627bc1239a670e8ba8814
scottycodehub <fangxia_li@yahoo.com>
2021-03-26 12:08:35 -0400

    initial project checkin

---
+++ src/com/eva/playground/JavaPlayground.java
@@ -0,0 +1,7 @@
+package com.eva.com.eva.playground;
+
+public class JavaPlayground {
+    public static void main(String[] args){
+        System.out.println("Hello-" + JavaPlayground.class.getName());
+    }
+}
---
+++ src/com/eva/playground/ScalaPlayground.scala
@@ -0,0 +1,6 @@
+package com.eva
+package com.eva.playground
+
+object ScalaPlayground extends App{
+  println(this.getClass.getName)
+}
